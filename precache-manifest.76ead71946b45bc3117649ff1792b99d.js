e5d54ddd6d7ca0fe904389be79464aa90c522856
Bartosz Staręga <bartosz.starega@gmail.com>
2020-08-05 17:39:30 +0200

    Updates

---
+++ precache-manifest.76ead71946b45bc3117649ff1792b99d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ad06c974308fbb22227c1419260063",
+    "url": "/owdr/index.html"
+  },
+  {
+    "revision": "2b9de9afc86e552e95eb",
+    "url": "/owdr/static/css/main.d0962ac6.chunk.css"
+  },
+  {
+    "revision": "3969b4b65dbfbdcfae44",
+    "url": "/owdr/static/js/2.3f9c9ccb.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/owdr/static/js/2.3f9c9ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9de9afc86e552e95eb",
+    "url": "/owdr/static/js/main.afd1566f.chunk.js"
+  },
+  {
+    "revision": "96584f2c886a4cedb753",
+    "url": "/owdr/static/js/runtime-main.e48f4128.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/owdr/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/owdr/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/owdr/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/owdr/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/owdr/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/owdr/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/owdr/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/owdr/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/owdr/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/owdr/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/owdr/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/owdr/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/owdr/static/media/Signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "42aa5dbd663f8b4c2034dac86a91c64c",
+    "url": "/owdr/static/media/hamburger_menu.42aa5dbd.svg"
+  },
+  {
+    "revision": "6ac2912e21748e633a0023fec99c9174",
+    "url": "/owdr/static/media/iks.6ac2912e.svg"
+  }
+]);
